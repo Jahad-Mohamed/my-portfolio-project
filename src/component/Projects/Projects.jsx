f970411408667cd7966e4b39646e5c767bd2ba6f
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-18 12:37:15 +0100

    save

--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -1,59 +1,56 @@
 import React from "react";
 import "./project.scss";
 import Pig from "/images/piggame.png";
+import Todo from "/images/todolist.png";
 const Projects = () => {
   return (
     <section id="projects" className="project">
-      <div>
+      <div className="project-section">
         <div className="project-title">My Projects</div>
 
         <div className="project__container">
-          <div className="project__containers">
-            <div className="container-card" id="container-card">
-              <div className="container-card__image">
-                <img src={Pig} alt="" className="container-card__image" />
-              </div>
-              <div className="container-card__title"> Game of Pig</div>
-              <p className="container-card__description">
-                Some quick example text to build on the div title and make up
-                the bulk of the div's content.
-              </p>
-              <button className="container-card__link" variant="primary">
-                <a href="https://thegameofpig.netlify.app/">
-                  Take me to Website
-                </a>
-              </button>
+          <div className="container-card" id="container-card">
+            <div className="container-card__image">
+              <img src={Pig} alt="" className="container-card__image" />
             </div>
-            <div className="container-card" id="container-card">
-              <div className="container-card__image">
-                <img src={Pig} alt="" className="container-card__image" />
-              </div>
-              <div className="container-card__title"> Game of Pig</div>
-              <p className="container-card__description">
-                Some quick example text to build on the div title and make up
-                the bulk of the div's content.
-              </p>
-              <button className="container-card__link" variant="primary">
-                <a href="https://thegameofpig.netlify.app/">
-                  Take me to Website
-                </a>
-              </button>
+            <div className="container-card__title"> Game of Pig</div>
+            <p className="container-card__description">
+              Some quick example text to build on the div title and make up the
+              bulk of the div's content.
+            </p>
+            <button className="container-card__link" variant="primary">
+              <a href="https://thegameofpig.netlify.app/">Take me to Website</a>
+            </button>
+          </div>
+
+          <div className="container-card" id="container-card">
+            <div className="container-card__image">
+              <img src={Todo} alt="" className="container-card__image" />
             </div>
-            <div className="container-card" id="container-card">
-              <div className="container-card__image">
-                <img src={Pig} alt="" className="container-card__image" />
-              </div>
-              <div className="container-card__title"> Game of Pig</div>
-              <p className="container-card__description">
-                Some quick example text to build on the div title and make up
-                the bulk of the div's content.
-              </p>
-              <button className="container-card__link" variant="primary">
-                <a href="https://thegameofpig.netlify.app/">
-                  Take me to Website
-                </a>
-              </button>{" "}
+            <div className="container-card__title"> To do List</div>
+            <p className="container-card__description">
+              This is my awesome to do list, created using HTML, CSS, and
+              Vanilla JavaScript.
+            </p>
+            <button className="container-card__link" variant="primary">
+              <a href="https://my-awesometodolist.netlify.app/">
+                Take me to Website
+              </a>
+            </button>
+          </div>
+
+          <div className="container-card" id="container-card">
+            <div className="container-card__image">
+              <img src={Pig} alt="" className="container-card__image" />
             </div>
+            <div className="container-card__title"> Game of Pig</div>
+            <p className="container-card__description">
+              Some quick example text to build on the div title and make up the
+              bulk of the div's content.
+            </p>
+            <button className="container-card__link" variant="primary">
+              <a href="https://thegameofpig.netlify.app/">Take me to Website</a>
+            </button>{" "}
           </div>
         </div>
       </div>
