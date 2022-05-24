2e41d7921894fbad935e94825f79b99d9378127b
Jahad Mohamed <jahadmohamed@live.co.uk>
2022-05-24 20:12:56 +0100

    updated porfolio

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -12,18 +12,18 @@ const About = () => {
 
         <Rotate top left delay={1500} duration={600}>
           <p className="about__text">
-            I'm a web developer focused to building creative, dynamtic react
-            applications and continuing to develop new skills in web
-            development. Currently looking for new employment opportunities{" "}
-            <br />
+            I'm a passionate web developer focused to building creative,
+            dynamtic reactive applications and continuing to develop new skills
+            in web development. Currently looking for new employment
+            opportunities ... <br />
             <br />
             In 2022 I graduated from BrainStation’s full-stack web development
             diploma program, where I strengthened my skills in HTML, CSS, and
             Javascript in a challenging bootcamp. I am most familiar with
             ReactJS as a front-end framework and NodeJS and ExpressJS as
-            back-end frameworks, but have also completed various personal
-            projects using skills in other technologies such as Nextjs, 3js
-            React Native and Firebase.
+            back-end, but have also completed various personal projects using
+            skills in other technologies such as Nextjs, 3js React Native and
+            Firebase.
           </p>{" "}
         </Rotate>
       </section>
--- src/component/Projects/Projects.jsx
+++ src/component/Projects/Projects.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./project.scss";
 import Pig from "/images/piggame.png";
 import Todo from "/images/todolist.png";
+import Expense from "/images/expenseTracker.png";
+import Amazon from "/images/amazon.png";
+
 const Projects = () => {
   return (
     <section id="projects" className="project">
@@ -41,15 +44,30 @@ const Projects = () => {
 
           <div className="container-card" id="container-card">
             <div className="container-card__image">
-              <img src={Pig} alt="" className="container-card__image" />
+              <img src={Expense} alt="" className="container-card__image" />
             </div>
-            <div className="container-card__title"> Game of Pig</div>
+            <div className="container-card__title"> Expense Tracker</div>
             <p className="container-card__description">
-              Some quick example text to build on the div title and make up the
-              bulk of the div's content.
+              This is an expense tracker created using React. Key features
+              include props, state, hooks.
             </p>
             <button className="container-card__link" variant="primary">
-              <a href="https://thegameofpig.netlify.app/">Take me to Website</a>
+              <a href="https://expensetracr.netlify.app/">Take me to Website</a>
+            </button>{" "}
+          </div>
+          <div className="container-card" id="container-card">
+            <div className="container-card__image">
+              <img src={Amazon} alt="" className="container-card__image" />
+            </div>
+            <div className="container-card__title"> Amazon Clone</div>
+            <p className="container-card__description">
+              This Amazon clone website has been built with Nextjs, Tailwind CSS
+              and Redux.
+            </p>
+            <button className="container-card__link" variant="primary">
+              <a href="https://amazon-clone-cyan-omega.vercel.app//">
+                Take me to Website
+              </a>
             </button>{" "}
           </div>
         </div>
